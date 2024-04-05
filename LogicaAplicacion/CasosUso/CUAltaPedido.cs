3fedae16dd0fc9d335d39cced1c61c448f15ba10
cxrals <55190406+cxrals@users.noreply.github.com>
2024-04-05 14:05:27 -0300

    pedidos

---
+++ LogicaAplicacion/CasosUso/CUAltaPedido.cs
@@ -0,0 +1,15 @@
+﻿using LogicaAplicacion.InterfacesCasosUso;
+using LogicaNegocio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.CasosUso {
+    public class CUAltaPedido : ICUAlta<Pedido> {
+        public void Alta(Pedido obj) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogicaDatos/Repositorios/RepositorioPedidos.cs
@@ -0,0 +1,23 @@
+﻿using LogicaNegocio.Dominio;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDatos.Repositorios {
+    public class RepositorioPedidos : IRepositorioPedidos {
+        public void Create(Pedido obj) {
+            //throw new NotImplementedException();
+        }
+
+        public void Delete(int id) {
+            //throw new NotImplementedException();
+        }
+
+        public void Update(Pedido obj) {
+            //throw new NotImplementedException();
+        }
+    }
+}
