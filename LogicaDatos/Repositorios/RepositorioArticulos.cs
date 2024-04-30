508fef4d2e73fe48c5640cb65b080467d32a5372
cxrals <55190406+cxrals@users.noreply.github.com>
2024-04-30 17:36:37 -0300

    duplicados

--- LogicaDatos/Repositorios/RepositorioArticulos.cs
+++ LogicaDatos/Repositorios/RepositorioArticulos.cs
@@ -16,8 +16,13 @@ namespace LogicaDatos.Repositorios {
         }
         public void Create(Articulo obj) {
             obj.EsValido();
-            Contexto.Articulos.Add(obj);
-            Contexto.SaveChanges();
+            if (!Contexto.Articulos.Any(a => a.Nombre == obj.Nombre) && !Contexto.Articulos.Any(a => a.CodigoProveedor == obj.CodigoProveedor)) {
+                Contexto.Articulos.Add(obj);
+                Contexto.SaveChanges();
+            } else {
+                throw new DuplicadoException("Ya existe un artículo con ese nombre y código de proveedor.");
+            }
+            
         }
 
         public void Delete(int id) {
@@ -40,8 +45,12 @@ namespace LogicaDatos.Repositorios {
 
         public void Update(Articulo obj) {
             obj.EsValido();
-            Contexto.Articulos.Update(obj);
-            Contexto.SaveChanges();
+            if (!Contexto.Articulos.Any(a => a.Nombre == obj.Nombre) && !Contexto.Articulos.Any(a => a.CodigoProveedor == obj.CodigoProveedor)) {
+                Contexto.Articulos.Update(obj);
+                Contexto.SaveChanges();
+            } else {
+                throw new DuplicadoException("Ya existe un artículo con ese nombre y código de proveedor.");
+            }
         }
     }
 }
