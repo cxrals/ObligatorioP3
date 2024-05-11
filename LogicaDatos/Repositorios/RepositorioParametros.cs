c8ffae1c2e6e68981439229ce5e6987f6fc7cea9
cxrals <55190406+cxrals@users.noreply.github.com>
2024-05-11 13:05:15 -0300

    tabla parametros

---
+++ LogicaDatos/Repositorios/RepositorioParametros.cs
@@ -0,0 +1,36 @@
+﻿using LogicaNegocio.Dominio;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDatos.Repositorios {
+    public class RepositorioParametros : IRepositorioParametros {
+        public ObligatorioContext Contexto { get; set; }
+        public RepositorioParametros(ObligatorioContext ctx) {
+            Contexto = ctx;
+        }
+        public void Create(Parametro obj) {
+            Contexto.Parametros.Add(obj);
+            Contexto.SaveChanges();
+        }
+
+        public void Delete(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Parametro FindById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public List<Parametro> GetAll() {
+            return Contexto.Parametros.ToList();
+        }
+
+        public void Update(Parametro obj) {
+            throw new NotImplementedException();
+        }
+    }
+}
